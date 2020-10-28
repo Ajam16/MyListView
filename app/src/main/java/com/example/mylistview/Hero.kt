91668791ac9d171b78c78c9467f355e18f56478b
Ajam16 <haikalazzamm04@gmail.com>
2020-10-28 19:52:02 +0700

    List View

---
+++ app/src/main/java/com/example/mylistview/Hero.kt
@@ -0,0 +1,10 @@
+package com.example.mylistview
+
+import android.accounts.AuthenticatorDescription
+import android.provider.ContactsContract
+
+data class Hero(
+    var photo: Int,
+    var name: String,
+    var description: String
+)
